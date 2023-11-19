29c9230f1282ddab9bb9d0b1ddc15a58df9942fe
BigT921 <zhangliangziqin@outlook.com>
2023-11-19 23:29:38 +0800

    新增教师、管理员类

--- Student.h
+++ Student.h
@@ -2,34 +2,34 @@
 #include <iostream>
 #include "Identity.h"
 
-	using namespace std;
+using namespace std;
 
-	class Student :Identity 
-	{
-		public:
+class Student :Identity 
+{
+	public:
 
-			//默认构造函数
-			Student();
+		//默认构造函数
+		Student();
 
-			//有参构造函数
-			Student(int id, string name, string pwd);
+		//有参构造函数
+		Student(int id, string name, string pwd);
 
-			//操作菜单
-			virtual void operMenu();
+		//操作菜单
+		virtual void operMenu();
 
-			//申请预约
-			void applyOrder();
+		//申请预约
+		void applyOrder();
 
-			//查看我的预约
-			void showMyOrder();
+		//查看我的预约
+		void showMyOrder();
 
-			//查看所有预约
-			void showAllOrder();
+		//查看所有预约
+		void showAllOrder();
 
-			//取消预约
-			void cancelOrder();
+		//取消预约
+		void cancelOrder();
 
-		public:
-			int m_id;
+	public:
+		int m_id;
 
-	};
+};
---
+++ Teacher.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include "Identity.h"
+
+using namespace std;
+
+class Teacher :Identity
+{
+	public:
+		//默认构造函数
+		Teacher();
+
+		//含参构造函数
+		Teacher(int id, string name, string pwd);
+
+		//操作菜单
+		virtual void operMenu();
+
+		//查看所有预约
+		void showAllOrder();
+
+		//审核预约
+		void validOrder();
+
+	public:
+		int m_EmpId;
+
+};
